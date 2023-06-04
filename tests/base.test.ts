db7ed54c45e51a5d3d06b576cec11f0402b919e3
Alexander Pirsig <gh@piscis.dev>
2023-06-04 13:55:00 +0200

    add base tests

---
+++ tests/base.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it } from 'vitest'
+import { $fetch, fetch, isDev } from '@nuxt/test-utils'
+
+describe('Base', () => {
+  it('index page is callable', async () => {
+    const doc = await $fetch('/')
+    expect(doc).toContain('DOCTYPE')
+  })
+
+  it('should remove x-powered-by', async () => {
+    const res = await fetch('/')
+    expect(res.headers.has('x-powered-by')).toBeFalsy()
+    expect(res.headers.has('X-Powered-By')).toBeFalsy()
+    expect(res.headers.has('content-type')).toBeTruthy()
+  })
+
+  if (isDev()) {
+    it('[dev] ensure vite client script is added', async () => {
+      expect(await $fetch('/')).toMatch('/_nuxt/@vite/client"')
+    })
+  }
+})
