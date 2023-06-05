87c5977e9bba9d3fd3189421e1aa8642bd68239a
Alex <gh@piscis.dev>
2023-06-05 19:00:24 +0200

    hookup nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: [
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image-edge',
+  ],
+  googleFonts: {
+    download: true,
+    base64: true,
+    families: {
+      Nunito: true,
+    },
+  },
+  nitro: {
+    routeRules: {
+      '/': { static: true },
+      '/_nuxt/**': {
+        headers: {
+          'cache-control': `public, max-age=${
+            60 * 60 * 24 * 30
+          }, stale-if-error=900, stale-while-revalidate=900, s-maxage=${
+            60 * 60 * 24 * 30
+          }`,
+        },
+      },
+    },
+  },
 })
