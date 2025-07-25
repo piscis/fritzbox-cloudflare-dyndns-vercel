3140ce585efb7396af4bb40fc81c86a06b45b2a5
Alex <gh@piscis.dev>
2025-07-25 11:29:20 +0200

    fix: fixes linter integration

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,4 @@
 import antfu from '@antfu/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-const baseConf = antfu()
-
-export default withNuxt(baseConf)
+export default withNuxt(antfu())
