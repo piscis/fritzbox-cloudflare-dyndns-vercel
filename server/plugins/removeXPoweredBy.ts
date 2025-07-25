c06247e13b54cb8eb716fdb50b728e02801f52a5
Alex <gh@piscis.dev>
2025-07-25 13:52:27 +0200

    removes xpoweredby plugin

--- server/plugins/removeXPoweredBy.ts
+++
@@ -1,8 +0,0 @@
-import type { RenderResponse } from 'nitropack'
-
-export default defineNitroPlugin((nitroApp) => {
-  nitroApp.hooks.hook('render:response', (response: RenderResponse) => {
-    delete response.headers['X-Powered-By']
-    delete response.headers['x-powered-by']
-  })
-})
