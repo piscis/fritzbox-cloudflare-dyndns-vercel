cbe5cc8ef6de2b0513bc5bc431377f55f257394d
Alexander Pirsig <gh@piscis.dev>
2025-07-25 10:41:04 +0200

    feat: modernize nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+const THIRTY_DAYS_IN_SECONDS = 60 * 60 * 24 * 30
+
 export default defineNuxtConfig({
+  compatibilityDate: '2025-06-06',
+  devtools: { enabled: true },
   modules: [
     '@unocss/nuxt',
     '@nuxtjs/google-fonts',
@@ -16,16 +19,21 @@ export default defineNuxtConfig({
       Nunito: true,
     },
   },
+  app: {
+    buildAssetsDir: '/_chunks/',
+    head: {
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+    },
+  },
   nitro: {
     routeRules: {
       '/': { static: true },
-      '/_nuxt/**': {
+      '/_chunks/**': {
         headers: {
-          'cache-control': `public, max-age=${
-            60 * 60 * 24 * 30
-          }, stale-if-error=900, stale-while-revalidate=900, s-maxage=${
-            60 * 60 * 24 * 30
-          }`,
+          'cache-control': `public, max-age=${THIRTY_DAYS_IN_SECONDS}, stale-if-error=900, stale-while-revalidate=900, s-maxage=${THIRTY_DAYS_IN_SECONDS}`,
         },
       },
     },
