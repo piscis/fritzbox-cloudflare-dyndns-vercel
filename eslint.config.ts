554b3931f46649dc3aafdac17af8d98e7f526eca
Alex <self@pirsig.net>
2025-07-25 15:25:51 +0200

    feat: ignore build directories

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,12 @@
 import antfu from '@antfu/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(antfu())
+export default withNuxt(antfu({
+  ignores: [
+    '.nuxt/**',
+    '.wrangler/**',
+    '.vercel/**',
+    '.output/**',
+    'node_modules/**',
+  ],
+}))
