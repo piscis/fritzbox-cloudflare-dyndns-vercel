dfda9b86b9421001b9f3700c990d5a10b4477157
Alex <self@pirsig.net>
2025-07-25 13:54:02 +0200

    feat: adds implementation via orpc

---
+++ server/router/procedures/health-check.ts
@@ -0,0 +1,45 @@
+import { os } from '@orpc/server'
+import { z } from 'zod'
+
+export enum HealthCheckStatusCodes {
+  OKAY = 'ok',
+  ERROR = 'error',
+}
+
+const HealthCheckSchema = z.object({
+  state: z.nativeEnum(HealthCheckStatusCodes),
+  timestamp: z.number().int().min(0).max(Number.MAX_SAFE_INTEGER),
+})
+
+export const healthCheck = os
+  .route({
+    method: 'GET',
+    path: '/health-check',
+    tags: ['Metrics'],
+    successStatus: 200,
+    summary: 'Health Check',
+    description: 'Check if the backend is healthy',
+    outputStructure: 'detailed',
+  })
+  .output(
+    z.object({
+      status: z.literal(200).describe('If the backend is healthy'),
+      body: HealthCheckSchema,
+    }),
+  )
+  .errors({
+    INTERNAL_SERVER_ERROR: {
+      description: 'Health check failed.',
+      status: 503,
+      message: 'Service Unavailable',
+    },
+  })
+  .handler(async () => {
+    return {
+      status: 200,
+      body: {
+        state: HealthCheckStatusCodes.OKAY,
+        timestamp: Date.now(),
+      },
+    }
+  })
---
+++ server/routes/api/[...].ts
@@ -0,0 +1,53 @@
+import { OpenAPIHandler } from '@orpc/openapi/fetch'
+import { OpenAPIReferencePlugin } from '@orpc/openapi/plugins'
+import { onError } from '@orpc/server'
+import { ZodSmartCoercionPlugin } from '@orpc/zod'
+import { ZodToJsonSchemaConverter } from '@orpc/zod/zod4'
+import { router } from '~~/server/router'
+import useLogger from '~~/server/utils/useLogger'
+
+const { logger } = useLogger()
+const { appName, appVersion } = useRuntimeConfig()
+
+const openAPIHandler = new OpenAPIHandler(router, {
+  eventIteratorKeepAliveEnabled: true,
+  eventIteratorKeepAliveInterval: 5000,
+  eventIteratorKeepAliveComment: 'keep-alive',
+  interceptors: [
+    onError((error) => {
+      logger.error(error)
+    }),
+  ],
+  plugins: [
+    new ZodSmartCoercionPlugin(),
+    new OpenAPIReferencePlugin({
+      schemaConverters: [
+        new ZodToJsonSchemaConverter(),
+      ],
+      specGenerateOptions: {
+        info: {
+          title: appName || 'Fritzbox Cloudflare DynDNS',
+          version: appVersion || '0.0.0',
+        },
+      },
+    }),
+  ],
+})
+
+export default defineEventHandler(async (event) => {
+  const req = toWebRequest(event)
+
+  const { matched, response } = await openAPIHandler.handle(req, {
+    prefix: '/api',
+    context: {
+      nitroContext: event,
+    },
+  })
+
+  if (!matched) {
+    setResponseStatus(event, 404, 'Not Found')
+    return 'Not Found'
+  }
+
+  return response
+})
