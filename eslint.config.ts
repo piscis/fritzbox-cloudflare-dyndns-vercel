d2c4e218f32b0c41213a729a539d6139bd1eefb9
Alex <gh@piscis.dev>
2025-07-25 11:03:18 +0200

    feat: configure nuxt + eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,6 @@
 import antfu from '@antfu/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(antfu)
+const baseConf = antfu()
+
+export default withNuxt(baseConf)
