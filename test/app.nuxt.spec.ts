a6fc1344e41dd87f287625480d69ce6c9b26cd6c
Alexander Pirsig <gh@piscis.dev>
2025-07-27 11:33:49 +0200

    use type alias to import app

--- test/app.nuxt.spec.ts
+++ test/app.nuxt.spec.ts
@@ -1,6 +1,6 @@
 import { mountSuspended } from '@nuxt/test-utils/runtime'
 import { expect, it } from 'vitest'
-import App from './../app/app.vue'
+import App from '~/app.vue'
 
 it('can also mount an app', async () => {
   const component = await mountSuspended(App, { route: '/' })
