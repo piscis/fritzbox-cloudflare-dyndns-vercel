0fe9dc6059f79a26ec5f0aff9d9c953fce88f254
Alexander Pirsig <gh@piscis.dev>
2025-07-27 11:33:17 +0200

    fix: removes unnessesary NuxtLayout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,20 +1,18 @@
 <template>
-  <NuxtLayout name="default">
-    <div
-      class="flex justify-center w-[95vw] lg:w-[80vw] 2xl:w-[60vw] max-w-1200 mx-auto h-screen items-center flex-col"
-    >
-      <div class="mb-5">
-        Beep Boop ... I'm a robot
-      </div>
-      <div class="mb-5">
-        <audio controls autoplay>
-          <source src="/sounds/modem-dial-up.mp3" type="audio/mpeg">
-        </audio>
-      </div>
-
-      <NuxtLink class="flex justify-center cursor-pointer font-light" href="https://github.com/piscis/fritzbox-cloudflare-dyndns-vercel/" target="_blank">
-        Learn more about this service: <Icon name="devicon:github" class="inline-block h-5 w-5 mx-2" />
-      </NuxtLink>
+  <div
+    class="flex justify-center w-[95vw] lg:w-[80vw] 2xl:w-[60vw] max-w-1200 mx-auto h-screen items-center flex-col"
+  >
+    <div class="mb-5">
+      Beep Boop ... I'm a robot
     </div>
-  </NuxtLayout>
+    <div class="mb-5">
+      <audio controls autoplay>
+        <source src="/sounds/modem-dial-up.mp3" type="audio/mpeg">
+      </audio>
+    </div>
+
+    <NuxtLink class="flex justify-center cursor-pointer font-light" href="https://github.com/piscis/fritzbox-cloudflare-dyndns-vercel/" target="_blank">
+      Learn more about this service: <Icon name="devicon:github" class="inline-block h-5 w-5 mx-2" />
+    </NuxtLink>
+  </div>
 </template>
